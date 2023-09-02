068a29ff2990e8872ff5aa2f72e2f85e839a552e
ToleonPrins <114396706+ToleonPrins@users.noreply.github.com>
2023-09-02 16:09:52 +0700

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Алексей Иванников on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
